5c0057b42eb8f2ae2287b8b8df843407c1b40bdb
yoyo3 <yao631029659@126.com>
2016-12-23 10:20:24 +0800

    修改了lazy的方式 改正了之前命令行 读取乱码问题

--- main.py
+++ main.py
@@ -10,29 +10,32 @@ app.config.from_object(DevConfig)
 # 传递给数据库
 db=SQLAlchemy(app)
 class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-    posts = db.relationship('Post', backref='user', lazy='subquery')
+    id=db.Column(db.Integer(),primary_key=True)
+    username=db.Column(db.String(255))
+    password=db.Column(db.String(255))
+    # lazy = subquery: 会在加载Post对象后，将与Post相关联的对象全部加载，这样就可以减少Query的动作，也就是减少了对DB的I / O操作。但可能会返回大量不被使用的数据，会影响效率。
+    # lazy = dynamic: 只有被使用时，对象才会被加载，并且返回式会进行过滤，如果现在或将来需要返回的数据量很大，建议使用这种方式。
+    posts=db.relationship('Post',backref='user',lazy='subquery')
+    def __init__(self,username):
+        self.username=username
 
-    def __init__(self, username):
-        self.username = username
     # 刚开始的时候 没写这句结果一用 users的时候老是报main 0x错误 搞了我一整天 我还以为是中文编码的问题呢 原来是错在这里
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class Post(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255))
-    text = db.Column(db.Text())
-    publish_date = db.Column(db.DateTime())
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    id=db.Column(db.Integer(),primary_key=True)
+    title=db.Column(db.String(255))
+    text=db.Column(db.Text())
+    push_date=db.Column(db.DateTime())
+    user_id=db.Column(db.Integer(),db.ForeignKey('user.id'))
 
-    def __init__(self, title):
-        self.title = title
+    def __init__(self,title):
+        self.title=title
 
     def __repr__(self):
-        return "<Post '{}'>".format(self.title)
+        return '<Post {}>'.format(self.title)
+
 
 
 
