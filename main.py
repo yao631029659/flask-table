951ee673d6cd13683c50d7a527f95eccd2627fbd
yoyo3 <yao631029659@126.com>
2016-12-23 08:27:02 +0800

    改进了上一个版本不能读取user.post

--- main.py
+++ main.py
@@ -10,28 +10,29 @@ app.config.from_object(DevConfig)
 # 传递给数据库
 db=SQLAlchemy(app)
 class User(db.Model):
-    id=db.Column(db.Integer(),primary_key=True)
-    username=db.Column(db.String(255))
-    password=db.Column(db.String(255))
-    posts=db.relationship(
-        'Post',
-        backref='user',
-        lazy='dynamic'
-    )
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='user', lazy='subquery')
+
+    def __init__(self, username):
+        self.username = username
     # 刚开始的时候 没写这句结果一用 users的时候老是报main 0x错误 搞了我一整天 我还以为是中文编码的问题呢 原来是错在这里
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class Post(db.Model):
-    id=db.Column(db.Integer(),primary_key=True)
-    title=db.Column(db.String(255))
-    text=db.Column(db.Text())
-    push_date=db.Column(db.DateTime())
-    user_id=db.Column(db.Integer(),db.ForeignKey('user.id'))
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+
+    def __init__(self, title):
+        self.title = title
 
     def __repr__(self):
-        return '<Post {}>'.format(self.title)
-
+        return "<Post '{}'>".format(self.title)
 
 
 
