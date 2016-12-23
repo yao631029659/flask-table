d5be99d4753cf925042f61776c57a1f4b493dc84
yoyo3 <yao631029659@126.com>
2016-12-23 08:14:05 +0800

    新建了Post表 增加了1对多的关系 但是还有部分错误

--- main.py
+++ main.py
@@ -13,9 +13,26 @@ class User(db.Model):
     id=db.Column(db.Integer(),primary_key=True)
     username=db.Column(db.String(255))
     password=db.Column(db.String(255))
+    posts=db.relationship(
+        'Post',
+        backref='user',
+        lazy='dynamic'
+    )
     # 刚开始的时候 没写这句结果一用 users的时候老是报main 0x错误 搞了我一整天 我还以为是中文编码的问题呢 原来是错在这里
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    title=db.Column(db.String(255))
+    text=db.Column(db.Text())
+    push_date=db.Column(db.DateTime())
+    user_id=db.Column(db.Integer(),db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.title)
+
+
 
 
 @app.route('/')
