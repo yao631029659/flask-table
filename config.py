6706537d622621229ceca12b90821575f125e7ba
yoyo3 <yao631029659@126.com>
2016-12-27 21:03:05 +0800

    表单测试通过

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 class Config(object):
     SECRET_KEY = '736670cb10a600b695a55839ca3a5aa54a7d7356cdef815d2ad6e19a2031182b'
 
-class DevConfig(object):
+# class DevConfig(object):  刚开始可以用这句 但是到了表单验证的时候就要改过来了
+class DevConfig(Config):
     #写了这句等下有提示
     DEBUG = True
     # 这个是sqlalchemy的uri
